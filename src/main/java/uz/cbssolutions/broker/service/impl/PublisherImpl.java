7e10db0a9db3b22db2cefacea911dd70a35e8d0c
Israiloff <stardustindark@gmail.com>
2023-01-31 15:05:21 +0500

    added: javadocs; fixed: refactoring issues;
    Signed-off-by: Israiloff <stardustindark@gmail.com>

--- src/main/java/uz/cbssolutions/broker/service/impl/PublisherImpl.java
+++ src/main/java/uz/cbssolutions/broker/service/impl/PublisherImpl.java
@@ -1,47 +1,44 @@
 package uz.cbssolutions.broker.service.impl;
 
-import jakarta.jms.Message;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessagePostProcessor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import uz.cbssolutions.broker.config.JmsSubPubConfig;
 import uz.cbssolutions.broker.service.Publisher;
+import uz.cbssolutions.broker.util.PublisherUtil;
 
 import java.io.Serializable;
 import java.util.Map;
 
+/**
+ * Implementation of message publisher contract.
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ArtemisPublisher implements Publisher {
+public class PublisherImpl implements Publisher {
 
     @Qualifier(JmsSubPubConfig.JMS_TEMPLATE)
     private final JmsTemplate jmsTemplate;
+    private final PublisherUtil publisherUtil;
 
-    private static MessagePostProcessor configureMessage(Map<String, Object> headers) {
-        return message -> {
-            headers.entrySet().forEach(header -> applyHeader(message, header));
-            return message;
-        };
-    }
-
-    @SneakyThrows
-    private static void applyHeader(Message message, Map.Entry<String, Object> header) {
-        message.setObjectProperty(header.getKey(), header.getValue());
-    }
-
+    /**
+     * @param topic           Name of target topic.
+     * @param model           Object to publish.
+     * @param headers         Additional headers to publish.
+     * @param <TRequestModel> Type of publishing model (body).
+     * @return End operation signal.
+     */
     @Override
     public <TRequestModel extends Serializable> Mono<Void> publish(String topic, TRequestModel model,
                                                                    Map<String, Object> headers) {
         log.debug("publish started for topic : {}", topic);
         return Mono.<Void>create(sink -> {
-            jmsTemplate.convertAndSend(topic, model, configureMessage(headers));
+            jmsTemplate.convertAndSend(topic, model, publisherUtil.applyHeaders(headers));
             sink.success();
         }).publishOn(Schedulers.boundedElastic());
     }
---
+++ src/main/java/uz/cbssolutions/broker/util/PublisherUtil.java
@@ -0,0 +1,39 @@
+package uz.cbssolutions.broker.util;
+
+import jakarta.jms.Message;
+import lombok.SneakyThrows;
+import org.springframework.jms.core.MessagePostProcessor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * Utilities for JMS message publisher.
+ */
+@Component
+public class PublisherUtil {
+
+    /**
+     * Multiple headers' applier.
+     *
+     * @param headers Target headers.
+     * @return Post processor of message.
+     */
+    public MessagePostProcessor applyHeaders(Map<String, Object> headers) {
+        return message -> {
+            headers.entrySet().forEach(header -> applyHeader(message, header));
+            return message;
+        };
+    }
+
+    /**
+     * Single header applier.
+     *
+     * @param message Target message.
+     * @param header  Target header.
+     */
+    @SneakyThrows
+    public void applyHeader(Message message, Map.Entry<String, Object> header) {
+        message.setObjectProperty(header.getKey(), header.getValue());
+    }
+}

35cdfbfad2f57a9fdc4023270dfc3aff79dc8349
Israiloff <stardustindark@gmail.com>
2023-08-30 13:53:00 +0500

    added: missing exchange type java docs;

--- src/main/java/io/github/israiloff/broker/config/ExchangeType.java
+++ src/main/java/io/github/israiloff/broker/config/ExchangeType.java
@@ -4,6 +4,12 @@ package io.github.israiloff.broker.config;
  * Type of exchange.
  */
 public enum ExchangeType {
+    /**
+     * Queue type.
+     */
     QUEUE,
+    /**
+     * Topic type.
+     */
     TOPIC
 }

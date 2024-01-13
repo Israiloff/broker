e65eb72220c8b47bd1bc5e67405ad998e304de7c
Israiloff <stardustindark@gmail.com>
2024-01-13 23:45:47 +0500

    fixed: subscriber's structure;

--- src/main/java/io/github/israiloff/broker/service/Subscriber.java
+++ src/main/java/io/github/israiloff/broker/service/Subscriber.java
@@ -1,5 +1,6 @@
 package io.github.israiloff.broker.service;
 
+import io.github.israiloff.broker.config.ExchangeType;
 import io.github.israiloff.broker.model.Message;
 import reactor.core.publisher.Mono;
 
@@ -26,6 +27,61 @@ public interface Subscriber<TRequestModel> {
     String getTopic();
 
     /**
+     * Defines the name of the sub queue.
+     *
+     * @return Name of the sub queue.
+     */
+    default String getSubQueue() {
+        return null;
+    }
+
+    /**
+     * Defines unique ID of the current JMS client.
+     * By default, value of the client ID will be the service name + random UUID.
+     * <p>
+     * E.g. dummy-service_3a187832-9d66-11ee-8c90-0242ac120002
+     *
+     * @return ID of JMS client.
+     */
+    default String getClientId() {
+        return null;
+    }
+
+    /**
+     * Enables subscription durability feature.
+     * Note that this feature works only for {@link #getExchangeType() exchange type} -
+     * {@link ExchangeType#TOPIC TOPIC}.
+     * Disabled by default.
+     *
+     * @return Durable flag.
+     */
+    default Boolean isDurable() {
+        return false;
+    }
+
+    /**
+     * Enables shared subscription feature.
+     * Note that this feature works only for {@link #getExchangeType() exchange type} -
+     * {@link ExchangeType#TOPIC TOPIC}.
+     * Disabled by default.
+     *
+     * @return Shared subscription flag.
+     */
+    default Boolean isShared() {
+        return false;
+    }
+
+    /**
+     * Defines a type of exchange of this subscription.
+     * Default value is {@link ExchangeType#TOPIC TOPIC}
+     *
+     * @return Exchange type of this subscription.
+     */
+    default ExchangeType getExchangeType() {
+        return ExchangeType.TOPIC;
+    }
+
+    /**
      * Incoming message handler.
      *
      * @param message Incoming message.
@@ -33,4 +89,3 @@ public interface Subscriber<TRequestModel> {
      */
     Mono<Void> handle(Message<TRequestModel> message);
 }
-

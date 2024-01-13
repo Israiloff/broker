661a23e57f994b23102b237f1d2c69701dfceb32
Israiloff <stardustindark@gmail.com>
2024-01-14 00:26:42 +0500

    fixed: subscriber config's refactoring issues;

--- src/main/java/io/github/israiloff/broker/config/SubscriberConfig.java
+++ src/main/java/io/github/israiloff/broker/config/SubscriberConfig.java
@@ -2,6 +2,7 @@ package io.github.israiloff.broker.config;
 
 import io.github.israiloff.broker.service.Subscriber;
 import io.github.israiloff.broker.util.SubscriberUtil;
+import jakarta.jms.MessageListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -20,25 +21,37 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SubscriberConfig {
 
-    private final SubscriberUtil util;
-
-    private final GenericApplicationContext applicationContext;
-
     /**
      * Multiple JMS message listener container beans creation point. Created message listener containers' count will be
      * equal to implementations of {@link Subscriber} (i.e. each {@link Subscriber} will have its own container).
      *
      * @param subscribers            List of implemented {@link Subscriber}.
      * @param messageListenerAdapter Default message listener adapter.
+     * @param subscriberUtil         Subscriber logic related utils.
+     * @param applicationContext     Spring application context instance.
      * @return Runner's bean.
      */
     @Bean
-    public ApplicationRunner runner(List<Subscriber> subscribers, MessageListenerAdapter messageListenerAdapter) {
+    public ApplicationRunner runner(List<Subscriber> subscribers, MessageListenerAdapter messageListenerAdapter,
+                                    SubscriberUtil subscriberUtil, GenericApplicationContext applicationContext) {
         return args -> subscribers.forEach(subscriber -> {
-            var container = util.createContainer(messageListenerAdapter, subscriber);
+            var container = subscriberUtil.createContainer(messageListenerAdapter, subscriber);
             var beanName = "messageListenerContainer_" + subscriber.getTopic();
             applicationContext.registerBean(beanName, SimpleMessageListenerContainer.class, () -> container);
             container.start();
         });
+    }
+
+    /**
+     * Default message listener adapter defined bean. Used for subscription purposes.
+     *
+     * @param listener Target message listener.
+     * @return Configured message listener adapter.
+     */
+    @Bean
+    public MessageListenerAdapter messageListenerAdapter(MessageListener listener) {
+        var adapter = new MessageListenerAdapter(listener);
+        adapter.setDefaultListenerMethod("onMessage");
+        return adapter;
     }
 }
